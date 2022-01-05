288a9eb0c0a4890b1bc83c9ea01bc579bbde67a0
Ugo, Okorie <support@convergroup.com>
2022-01-05 15:16:03 +0100

    version 1

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+// // function addNums($a = 0, $b = 0, $c = 0) {
+// 	$c = $a + $b;
+// 	print($c);
+
+// 	return $a + $b;
+// }
+
+// print(addNums(10, 15, 5));
+// print('<br>');
+// print(addNums(5, 158));
+
+function register($slogan = 'i love php') {
+	$name = 'Jack';
+	$lastname = 'sparrow';
+
+	return "$name $lastname $slogan";
+}
+
+?>
